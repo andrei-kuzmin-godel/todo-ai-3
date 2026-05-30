11fcb82fe1a25671a58c0f144ec86cd050e7b2b2
andrei-kuzmin-godel <andrei-kuzmin-godel@users.noreply.github.com>
2026-05-30 07:59:46 +0000

    deploy: 6c2d4b755faf3cf58fd60d15bc01dd8347398232

---
+++ _next/static/plRXEHzmxapqw5qiu0IBI/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/todo-ai-3//_next/:path+",
+        "destination": "/todo-ai-3/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
